a37a0ff8027971c5f36570270f9ad73672835663
sonika <sonikak004@gmail.com>
2023-05-02 13:55:52 +0100

    Fix required changes in js

--- index.js
+++ index.js
@@ -39,6 +39,9 @@ const UI = {
     const row = document.createElement('tr');
 
     row.innerHTML = `
+
+
+    
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>

1c914b6318a6422e05e253cd026d20cc44dfd887
sonika <sonikak004@gmail.com>
2023-05-03 12:12:46 +0100

    Fix linters

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ class UI {
   static deleteBook(el) {
     if (el.classList.contains("delete")) {
       el.parentElement.parentElement.remove();
-      
+
     }
   }
 
@@ -43,8 +43,8 @@ class UI {
     const form = document.querySelector("#book-form");
     container.insertBefore(div, form);
 
-    // Vanish in 2.5 seconds
-    setTimeout(() => document.querySelector(".alert").remove(), 2500);
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
   }
 
   static clearFields() {
@@ -74,45 +74,37 @@ class Store {
 
 }
 
-// Event: Display Books
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
-// Event: Add a Book
+
 document.querySelector("#book-form").addEventListener("submit", (e) => {
   // Prevent actual submit
   e.preventDefault();
 
-  // Get form values
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
 
-  // Validate
   if (title === "" || author === "") {
     UI.showAlert("Please fill in all fields", "danger");
   } else {
-    // Instatiate book
+
     const book = new Book(title, author);
 
     // Add Book to UI
     UI.addBookToList(book);
 
-    // Add book to store
     Store.addBook(book);
 
-    // Show success message
     UI.showAlert("Book Added", "success");
 
-    // Clear fields
     UI.clearFields();
   }
 });
 
 // Event: Remove a Book
 document.querySelector("#book-list").addEventListener("click", (e) => {
-  // Remove book from UI
-  UI.deleteBook(e.target);
 
-  // Remove book from loacal storage
+  UI.deleteBook(e.target);
 
   // Show success message
   UI.showAlert("Book Removed", "success");

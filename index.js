7a8c3edb03507217632ecb68290fd4861299ffc2
sonika <sonikak004@gmail.com>
2023-05-05 13:58:06 +0100

    Modify js to display page when clicked

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-// const bookList = document.getElementById('book-list');
-// const addBook = document.getElementById('add-book');
-// const contact = document.getElementById('contact');
+const itemOne = document.getElementById('item1');
+const itemTwo = document.getElementById('item2');
+const itemThree = document.getElementById('item3');
+const bookSection = document.getElementById('book-section');
+const addSection = document.getElementById('add-section');
+const contactSection = document.getElementById('contact-section');
+
+// when link is clicked the rest of the sections are hidden using the class
+
+itemOne.addEventListener('click', () => {
+  bookSection.classList.remove('hidden');
+  addSection.classList.add('hidden');
+  contactSection.classList.add('hidden');
+});
+
+itemTwo.addEventListener('click', () => {
+  addSection.classList.remove('hidden');
+  bookSection.classList.add('hidden');
+  contactSection.classList.add('hidden');
+});
+
+itemThree.addEventListener('click', () => {
+  contactSection.classList.remove('hidden');
+  bookSection.classList.add('hidden');
+  addSection.classList.add('hidden');
+});
